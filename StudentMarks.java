4a1d24c74a7811c6a2b81f9f21634f1a36c3bb65
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2024-07-14 22:52:25 +1000

    print method changed.
    given a number for student while printing the marks.

--- StudentMarks.java
+++ StudentMarks.java
@@ -13,34 +13,63 @@ public class StudentMarks
 {
  
     public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the assignment name: ");
+        String assignmentName = scanner.nextLine();
+        
+        
         
         //defining the number of students 
         int numberOfStudents = 30;
         
         //creating array to store the marks
         float[] marks = new float[numberOfStudents];
-        Scanner scanner = new Scanner(System.in);
         
         // promt for users to enter the marks 
          System.out.println("Enter the marks of all students: ");
          
          
-         for (int i = 0; i< numberOfStudents; i++){
+         //for (int i = 0; i< numberOfStudents; i++){
              //let's create a program to check the validity of the marks given.
+           //  float checkMarks = scanner.nextFloat();
+             //if (checkMarks <=0 || checkMarks > 30){
+               // System.out.println("Invalid marks, Please enter the valid marks");
+            //}
+            //marks[i]= checkMarks;
              
-             float checkMarks = scanner.nextFloat();
-             if (checkMarks <=0 || checkMarks > 30){
-                System.out.println("Invalid marks, Please enter the valid marks");
-            }
-            marks[i]= checkMarks;
-             
-            };
+           // };
             
-         System.out.println("Entered Marks: ");
-         for (int j = 0; j<numberOfStudents; j++ ){
-          System.out.println(marks[j]);
+             for (int i = 0; i < numberOfStudents; i++) {
+            while (true) {
+                // Prompt for mark
+                System.out.print("Enter the marks for student " + (i + 1) + ": ");
+                float mark = scanner.nextFloat();
+
+                // Checking if mark is valid
+                if (mark > 0 || mark <= 30) {
+                    marks[i] = mark; // valdating the user inputs 
+                    break; // exit loop if user has provided the valid mark
+                } else {
+                    System.out.println("Invalid mark! Please enter a mark between 0 and 30.");
+                }
+            }
+        }
+          
+            
+         //System.out.println("Entered Marks: ");
+         //for (int j = 0; j<numberOfStudents; j++ ){
+          //System.out.println(marks[j]);
+        //}
+        
+         // Printing  entered marks
+        System.out.println("\nEntered Marks:");
+        for (int j = 0; j < numberOfStudents; j++) {
+            System.out.println("Student " + (j + 1) + ": " + marks[j]);
         }
         
+        //finding out and print
         //let's create a code for mean first and go for the 
         // highest and lowest later.
         //mean = sum of all marks /number of marks
