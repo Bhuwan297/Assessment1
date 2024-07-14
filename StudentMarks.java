5d8a77d03f5988fcb513a8995fde5db890168b6b
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2024-07-14 18:32:58 +1000

    Validation for Marks.
    an error message shows up when entered the invalid marks.

--- StudentMarks.java
+++ StudentMarks.java
@@ -18,13 +18,25 @@ public class StudentMarks
         float[] marks = new float[numberOfStudents];
         Scanner scanner = new Scanner(System.in);
         
-         System.out.println("Enter the marks of students: ");
+         System.out.println("Enter the marks of all students: ");
          
-         for (int i = 0; i> numberOfStudents; i++){
-            marks[i]= scanner.nextFloat();
-            
-            
+         
+         for (int i = 0; i< numberOfStudents; i++){
+             //let's create a program to check the validity of the marks given.
+             
+             float checkMarks = scanner.nextFloat();
+             if (checkMarks <=0 || checkMarks > 30){
+                System.out.println("Invalid marks, Please enter the valid marks");
+            }
+            marks[i]= checkMarks;
+             
             };
+            
+         System.out.println("Entered Marks: ");
+         for (int j = 0; j<numberOfStudents; j++ ){
+          System.out.println(marks[j]);
+        }
+         
          
          
         
